cda972013524aadd8fb99233148c880b1e508259
onebitjoy <95994085+onebitjoy@users.noreply.github.com>
2025-05-23 11:27:34 +0530

    REFRESH from a new machine

--- src/_root/pages/HomePage.tsx
+++ src/_root/pages/HomePage.tsx
@@ -1,10 +1,36 @@
-import { Button } from "@/components/ui/button"
+import Loading from "@/components/Loading";
 
 function HomePage() {
+  const isPostLoading = true;
+
   return (
-    <main className="dark:bg-black dark:text-white grow">
-      <div>Hello homepage it is</div>
-      <Button variant="outline">Button</Button>
+    <main className="flex justify-center dark:bg-black border border-red-400 w-full dark:text-white">
+      {/* post feed section */}
+      <div className="flex flex-col lg:mr-8 border border-blue-400/50">
+
+        {/* Stories */}
+        <div className="gap-2 grid grid-flow-col auto-cols-auto mx-auto w-dvw sm:w-120 md:w-140 overflow-x-auto overscroll-contain">
+          {
+            Array
+              .from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
+              .map(
+                (iter, index) => <div key={index} className="place-items-center grid bg-red-400/20 rounded w-40 h-20 gradient">
+                  {iter}
+                </div>
+              )
+          }
+        </div>
+
+        {/* Post Feed */}
+        <div className="border w-full min-h-max grow">
+          {isPostLoading ? <Loading /> : <p className="text-white">Post loaded</p>}
+        </div>
+      </div>
+
+      {/* followers section */}
+      <div className="hidden xl:block border border-yellow-400/50 w-80">
+        followers
+      </div>
     </main>
   )
 }
---
+++ src/components/Loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="place-items-center grid w-full h-full">
+      <div className="loadingbox"></div>
+    </div>
+  )
+}
